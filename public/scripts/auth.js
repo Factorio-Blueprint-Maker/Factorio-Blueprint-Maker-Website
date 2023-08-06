47ca4d483dc4e27b9126dc9898a59c0111ccb7bc
Liam Nordvall <liam_nordvall@hotmail.com>
2023-08-06 22:38:26 +0200

    email verification backend
    Added email verification backend

--- public/scripts/auth.js
+++ public/scripts/auth.js
@@ -22,54 +22,6 @@ if (signinBtn) {
     });    
 }
 
-// Signup
-
-const nextStepEmail = document.querySelector(".next-step-email");
-const nextStepPassword = document.querySelector(".next-step-password");
-
-const emailpage = document.querySelector(".email-container");
-const passwordpage = document.querySelector(".password-container");
-const verificationpage = document.querySelector(".verification-container");
-
-if (nextStepEmail) {
-
-    nextStepEmail.addEventListener("click", () => {
-
-        const username = document.getElementById("username").value;
-        const email = document.getElementById("email").value;
-
-        // check if the input fields are empty
-        if (email.trim() === "" || username.trim() === "") {
-            console.log("there's empty input fields");
-        }
-        else {
-            emailpage.style.display = "none";
-            passwordpage.style.display = "block";
-
-            nextStepPassword.addEventListener("click", () => {
-                
-                const password = document.getElementById("password").value;
-                const confirm_password = document.getElementById("confirm-password").value;
-
-                if (password == confirm_password) {
-                    createUserWithEmailAndPassword(auth, email, password)
-                    .then((credential) => {
-
-                        passwordpage.style.display = "none";
-                        verificationpage.style.display = "block";    
-
-                        console.log(auth.currentUser);
-                    }).catch((error) => {
-                        console.error(error.message);
-                    })
-                    } else {
-                        console.log("hey");
-                }
-
-            })
-        }
-    })
-}
 
 // Signup with Google
 const signinWithGoogle = document.querySelector(".signinWithGoogle");
@@ -101,6 +53,7 @@ if(signinWithGoogle) {
     });
 }
 
+
 // Logout User
 function logoutUser() {
     signOut(auth).then(() => {
@@ -114,24 +67,83 @@ function logoutUser() {
 export { logoutUser };
 
 
-// check auth state
-function checkAuthState() {
-
-    onAuthStateChanged(auth, (user) => {
-
-        const signinLink = document.getElementById("signin-link");
-        const profile = document.getElementById("profile");
-
-        if (user) {
-            signinLink.style.display = 'none';
-            profile.style.display = 'block'; 
+document.addEventListener('DOMContentLoaded', () => {
+    checkAuthState(); 
+});
+  
+  async function checkAuthState() {
+    await onAuthStateChanged(auth, (user) => {
+      const signinLink = document.getElementById("signin-link");
+      const profile = document.getElementById("profile");
+  
+      if (!user) {
+        signinLink.style.display = "block";
+        profile.style.display = "none";
+      } else {
+        if (user.emailVerified) {
+          signinLink.style.display = "none";
+          profile.style.display = "block";
         } else {
-            signinLink.style.display = 'block';
-            profile.style.display = 'none';
+          // User is signed in but not verified
+          // You can handle this case accordingly, for example, by showing a message
+          signinLink.style.display = "block";
+          profile.style.display = "none";
         }
+      }
     });
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const signupBtn = document.querySelector(".signup-button");
+
+if (signupBtn) {
+  signupBtn.addEventListener("click", () => {
+    signupUser();
+  });
 }
 
+async function waitForEmailVerification(user) {
+    while (!user.emailVerified) {
+      await user.reload();
+      await new Promise((resolve) => setTimeout(resolve, 5000)); 
+    }
+}
+  
+async function signupUser() {
+    try {
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
+  
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+  
+      // Sending email verification for the newly created user.
+      await sendEmailVerification(user);
+  
+      console.log("Email verification sent successfully!");
+  
+      // Wait until the email is verified
+      await waitForEmailVerification(user);
+  
+      await checkAuthState();
 
-// Automatically check auth state when the page loads
-document.addEventListener('DOMContentLoaded', checkAuthState);
+      console.log("Successfull Signup!!");
+
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
